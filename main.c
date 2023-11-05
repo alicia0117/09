c1fd77c9077f0a664695c78c4c2b4d74463326f0
alicia0117 <alicia0117@naver.com>
2023-11-05 22:36:25 +0900

    실습 03_1

--- main.c
+++ main.c
@@ -7,14 +7,18 @@ int main(int argc, char *argv[])
 {
     int i;
     int grade [N_STUDENT];
-    printf(" input 5 scores: ");
+    int score [N_STUDENT];
     
     for (i=0; i<N_STUDENT; i++)
-        scanf("%d" , &grade[i]);
+       grade[i] =  rand()%100 + 1;
+       
+    for (i=0; i<N_STUDENT; i++)
+    score [i] = grade[i];
+       
         
     for(i=0; i<N_STUDENT; i++)
-       printf("grade[%d] = %d\n");
+       printf("score[%d] = %d (grade:%i)\n", i, score[i],grade[i]); 
     
-  system("PAUSE");	
-  return 0;
+     system("PAUSE");	
+     return 0;
 }
